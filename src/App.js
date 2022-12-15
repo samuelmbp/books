76048c59073f2a6516e6b8d776ecc1d83e68f3c9
Samuel Raducan <samuel.thembp@gmail.com>
2022-12-15 21:01:57 +0000

    BookCreate component can receive new titles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 const App = () => {
+	const [books, setBooks] = useState([]);
+
+	const createBook = (title) => {
+		console.log('Need to add book with: ', title);
+	}
+
 	return (
 		<div>
-			<BookCreate />
 			<BookList />
+			<BookCreate  onCreate={createBook} />
 		</div>
 	);
 };
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function BookCreate() {
+function BookCreate({ onCreate }) {
+	const [title, setTitle] = useState('');
+
+	const handleChange = (e) => {
+		setTitle(e.target.value);
+	}
+
+ const handleSubmit = (e) => {
+		e.preventDefault();
+		onCreate(title);
+		setTitle(''); 
+ }
+
 	return (
-		<div>BookCreate</div>
-	)
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>Book Title: </label>
+				<input  
+					type="text"  
+					value={title} 
+					onChange={handleChange}
+				/>
+				<button onSubmit={handleSubmit}>Add Book</button>
+			</form>
+		</div>
+	) 
 }
 
-export default BookCreate
+export default BookCreate 
