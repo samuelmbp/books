1b944cf80f688e2b0d9de8b4766197c04b5abad4
Samuel Raducan <samuel.thembp@gmail.com>
2022-12-16 17:07:28 +0000

    Create the form for the BookEdit component and shows the title as default

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from "react"
 
-function BookEdit() {
+function BookEdit({ book }) {
+	const [title, setTitle] = useState(book.title);
+
+	const handleChange = (e) => {
+		setTitle(e.target.value);
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log('New title is:', title);
+ }
+
 	return (
-		<div>BookEdit</div>
+		<div>
+			<form onSubmit={handleSubmit} className="book-edit">
+				<label>Title</label>
+				<input 
+					className="input" 
+					value={title} 
+					onChange={handleChange}
+				/>
+				<button className="button is-primary">Save</button>
+			</form>
+		</div>
 	)
 }
 
